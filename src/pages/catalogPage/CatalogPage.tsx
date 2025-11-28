49b7b1eaa76e976165fa78eedc3ba2b52aed13c4
kabutoedit <kolya2007niger@gmail.com>
2025-11-28 16:29:27 +0600

    another fix bag

--- src/pages/catalogPage/CatalogPage.tsx
+++ src/pages/catalogPage/CatalogPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Category from '../category/Category'
+// import Category from '../category/Category'
 import SubCategory from '../products/Products'
 import ProductsPage from '../subCategory./SubCategory'
 import { ProductsType } from '../../types'
